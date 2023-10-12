4d3aae92d70cb52a2f077e0329f3e53f868ca227
Joao Dantas <joaoguidev@gmail.com>
2023-10-12 14:43:38 -0700

    before branch

--- app/root.jsx
+++ app/root.jsx
@@ -1,33 +1,34 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+import { cssBundleHref } from "@remix-run/css-bundle"
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import stylesheet from "~/tailwind.css";
+   Links,
+   LiveReload,
+   Meta,
+   Outlet,
+   Scripts,
+   ScrollRestoration,
+} from "@remix-run/react"
+import stylesheet from "~/tailwind.css"
 
-export const links = () => [
-  { rel: "stylesheet", href: stylesheet },
-];
+export const links = () => [{ rel: "stylesheet", href: stylesheet }]
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+   return (
+      <html lang="en">
+         <head>
+            <meta charSet="utf-8" />
+            <meta
+               name="viewport"
+               content="width=device-width, initial-scale=1"
+            />
+            <Meta />
+            <Links />
+         </head>
+         <body>
+            <Outlet />
+            <ScrollRestoration />
+            <Scripts />
+            <LiveReload />
+         </body>
+      </html>
+   )
 }
---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+   plugins: ["prettier-plugin-tailwindcss"],
+   tailwindFunctions: ["clsx"],
+   tabWidth: 3,
+   semi: false,
+   bracketSameLine: true
+};
